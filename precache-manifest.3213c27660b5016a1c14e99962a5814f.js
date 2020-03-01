5cd5c9190bf0172a8e547217e1a39eb1b567661c
harish <harishbohare@gmail.com>
2020-03-01 17:34:42 +0530

    Updates

---
+++ precache-manifest.3213c27660b5016a1c14e99962a5814f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7ec93a4d8e68ccca8f753a7b9116c6",
+    "url": "/shopping-site/index.html"
+  },
+  {
+    "revision": "a742ef4c8e0431f4c332",
+    "url": "/shopping-site/static/css/main.5e719dbe.chunk.css"
+  },
+  {
+    "revision": "3b8a8664715147bb9a91",
+    "url": "/shopping-site/static/js/2.b31707b2.chunk.js"
+  },
+  {
+    "revision": "a742ef4c8e0431f4c332",
+    "url": "/shopping-site/static/js/main.ad822283.chunk.js"
+  },
+  {
+    "revision": "f38395717800bcfa8e7b",
+    "url": "/shopping-site/static/js/runtime-main.818eb08d.js"
+  }
+]);
